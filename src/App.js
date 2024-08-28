890145c0eb1835d6acacd4b5238837e9ead55f12
Matthew Hamblin <matthamblin60@gmail.com>
2024-08-27 21:09:48 -0400

    added starter styling

--- src/App.js
+++ src/App.js
@@ -12,12 +12,6 @@ import Logo from './pictures/Logo .svg'
 function App() {
   return (
     <>
-    <meta name="description" content="Little Lemon Restaurant, Chicago"/>
-    <meta name="og:title" content="Little Lemon"/>
-    <meta name="og:description" content="We are a family owned Mediterranean restaurant, focused on traditional recipes serviced with a modern 
-    twist."/>
-    <meta name="og:image" content={Logo
-    }/>
     <Header />
     <Nav />
     <Main />
--- src/Nav.js
+++ src/Nav.js
@@ -1,19 +1,19 @@
 import logo from "./pictures/Logo .svg"
-import menuIcon from "./pictures/🦆 icon _hamburger menu.svg"
 
 const Nav = () =>{
     return(
         <nav>
-            <img src={menuIcon} alt ="logo failed" />
-            <img src={logo} alt ="logo failed" />
-            <ul>
-                <li><a href="">Home</a></li>
-                <li><a href="">About</a></li>
-                <li><a href="">Menu</a></li>
-                <li><a href="">Reservations</a></li>
-                <li><a href="">Order Online</a></li>
-                <li><a href="">Login</a></li>
-            </ul>
+            <img className="logo" src={logo} alt ="logo" />
+            <>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Menu</a></li>
+                    <li><a href="#">Reservations</a></li>
+                    <li><a href="#">Order Online</a></li>
+                    <li><a href="#">Login</a></li>
+                </ul>
+            </>
         </nav>
     );
 }
